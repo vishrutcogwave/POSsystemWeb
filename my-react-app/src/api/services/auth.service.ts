84895dd6c65aa2cf7f54881656900cb8fa4de2f8
vishrutcogwave <vih5gamer@gmail.com>
2026-06-22 18:28:09 +0530

    updated code

--- my-react-app/src/api/services/auth.service.ts
+++ my-react-app/src/api/services/auth.service.ts
@@ -1,13 +1,16 @@
 import type { LoginRequest } from "../../types/types";
 import api from "../axios";
-
 export const login = async (data: LoginRequest): Promise<any> => {
-  const response = await api.post<any>("/api/POS/BtnSubmitLogin", data);
+  const response = await api.post("/api/POS/BtnSubmitLogin", data);
 
-  // Save token for future API calls
-  localStorage.setItem("token", response.data.user.token);
-    localStorage.setItem("branch", data.branch_code);
+  console.log("Login API Response:", response.data);
 
-  return response.data;
+  localStorage.setItem(
+    "token",
+    response.data.data.user.token
+  );
+
+  localStorage.setItem("branch", data.branch_code);
+
+  return response.data.data;
 };
-
